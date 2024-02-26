67642d52d692032a7c7e2b4215e94acedb8e7e7c
dlibberton <drew.libberton@gmail.com>
2024-02-26 10:14:33 -0500

    fixed pscyopg and postgress connection

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://dal@localhost/students"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://dal:root@localhost:5432/school"
+
+
 
 db = SQLAlchemy(app)
 
@@ -12,7 +14,7 @@ class Student(db.Model):
     first_name = db.Column(db.String(50))
     last_name = db.Column(db.String(50))
     age = db.Column(db.Integer)
-    subject = db.Column(db.Integer(1))
+    subject = db.Column(db.Integer)
     
 class Subjects(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -27,13 +29,17 @@ class Teachers(db.Model):
     
     
     
-@app.route('api/v1/student', methods=['GET'])
+@app.route('/api/v1/student/', methods=['GET'])
 def get_students():
-    students = Student.query.all()
+    try:
+        students = Student.query.all()
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+        return (f"Unexpected {err=}, {type(err)=}")
+    
     student_list = [
         {'id': student.id, 'first_name': student.first_name, 'last_name': student.last_name, 'age': student.age, 'subject': student.subject}
-        for student in students
-    ]
+        for student in students]
     response = jsonify(student_list)
     return response
 
